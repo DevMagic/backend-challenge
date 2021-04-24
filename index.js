01b40114b4c086fea9d00fc84352f1255d02abd0
thmtvz <thmtvzu@gmail.com>
2021-04-23 23:23:44 -0300

    Correçao importante

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ app.get('/', (req, res) => {
   res.send('bem vindo!');
 })
 
+app.get('*', (req, res) =>{
+  res.json({message: "desulpe, nao encontrado"})
+})
+
+app.post('*', (req, res) =>{
+  res.json({message: "desulpe, nao encontrado"})
+})
+
+app.put('*', (req, res) =>{
+  res.json({message: "desulpe, nao encontrado"})
+})
+
+app.delete('*', (req, res) =>{
+  res.json({message: "desulpe, nao encontrado"})
+})
 
 app.listen(port, () =>{
     console.log('app escutando no port: ', port);
