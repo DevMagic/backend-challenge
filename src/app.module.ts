d129947e4ccd9a5e34ad01cf1cb4536b06d64a84
Victor Campelo <victor_campelo@outlook.com>
2021-05-03 22:02:05 -0300

    feat (CONFIG): add winston to store logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './Configs/typeorm.config';
+import { WinstonModule } from 'nest-winston';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { winstonConfig } from './configs/winston.config';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { PlayerModule } from './player/player.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    PlayerModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
