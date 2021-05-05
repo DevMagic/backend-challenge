e772f6e476ad13da52c96408de0a8f3db9733acd
Victor Campelo <victor_campelo@outlook.com>
2021-05-05 09:47:02 -0300

    feat (CONFIG): add joi validation to environment variables and replace typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,29 @@ import { WinstonModule } from 'nest-winston';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { winstonConfig } from './configs/winston.config';
 import { LoggerInterceptor } from './interceptors/logger.interceptor';
-import { PlayerModule } from './player/player.module';
+import { SummonerModule } from './player/summoner.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        TYPEORM_CONNECTION: Joi.string().required(),
+        TYPEORM_USERNAME: Joi.string().required(),
+        TYPEORM_PASSWORD: Joi.string().required(),
+        TYPEORM_DATABASE: Joi.string().required(),
+        TYPEORM_HOST: Joi.string().required(),
+        TYPEORM_PORT: Joi.number().required(),
+        TYPEORM_SYNCHRONIZE: Joi.boolean().required(),
+        NODE_ENV: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    DatabaseModule,
     WinstonModule.forRoot(winstonConfig),
-    PlayerModule,
+    SummonerModule,
   ],
   controllers: [],
   providers: [
