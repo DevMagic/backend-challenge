6861bbd19f2d9821f568c33b5eb6803a1264d01b
Victor Campelo <victor_campelo@outlook.com>
2021-05-06 15:04:41 -0300

    build (SCRIPT): remove migration execution and remove watch mode to start

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 ./scripts/wait-for-it.sh -t 0 pgsql:5432
-yarn typeorm migration:run
+# yarn typeorm migration:run
 
 if [ "${NODE_ENV}" = 'test' ]; then
 	echo "Running Test";
 	yarn test
 else 
-	echo "Running Dev Mode"
-    yarn start:dev
+	echo "Running Application";
+	yarn build
+    yarn start
 fi
