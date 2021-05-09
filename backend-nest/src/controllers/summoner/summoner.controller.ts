a7fb3d428351e89e428c64fee7beb8ff8443edeb
LucasBissa <lucasbomfimddt@gmail.com>
2021-05-09 16:33:08 -0300

    fix null body in post and put

--- backend-nest/src/controllers/summoner/summoner.controller.ts
+++ backend-nest/src/controllers/summoner/summoner.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Post, Put, Body, Param} from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Body, Param, BadRequestException} from '@nestjs/common';
 import { SummonerService } from '../../services/summoner/summoner.service'
 import { SummonerRequest } from 'src/dto/summoner/summonerDTO';
 
@@ -19,11 +19,17 @@ export class SummonerController {
 
     @Post()
     createSummoner(@Body() {summonerName}: SummonerRequest){
+        if(!summonerName)
+            throw new BadRequestException("summonerName should not be null");
+
         return this.summonerService.createSummoner(summonerName);
     }
 
     @Put(":id")
     updateSummoner(@Param('id') id: number, @Body() summoner: SummonerRequest){
+        if(!(summoner.summonerName && summoner.summonerLevel))
+            throw new BadRequestException("summonerName and summonerLevel should not be null")
+
         return this.summonerService.updateSummoner(id,summoner);
     }
 
