6d1090d61e6316dd453015d56285fe63d6781a6a
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-06-13 20:24:44 -0300

    add routes.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { routes } from './routes.js';
 
 const server = express();
 
-server.get('/', (request, response) => {
-  response.send('Hello World');
-});
+server.use(routes);
 
 server.listen(process.env.PORT || 3000);
